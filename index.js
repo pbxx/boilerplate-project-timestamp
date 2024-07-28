375fbc014987c29c69d51c40daa106782f52ce05
Donavan Lewis <airlew@gmail.com>
2024-07-27 22:38:40 -0600

    setup microservice

--- index.js
+++ index.js
@@ -2,31 +2,35 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require("express")
+var app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors")
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+	res.sendFile(__dirname + "/views/index.html")
+})
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
+// your first API endpoint...
+app.get("/api/:time", function (req, res) {
+	if (req.params.time) {
+    console.log(req.params.time.split('-').length, req.params.time)
+		const date = new Date(req.params.time.split('-').length > 1 ? req.params.time : parseInt(req.params.time) )
+		const outObj = { unix: date.valueOf(), utc: date.toUTCString() }
+		res.json(outObj)
+	} else {
+		res.status(400).send("Malformed request")
+	}
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+	console.log("Your app is listening on port " + listener.address().port)
+})
